ee8ee64ad61b34dbbc0e39a5ab41f3decd650f84
namnguyen <toilanam97@gmail.com>
2019-09-04 16:53:23 +0700

    Future calculate

---
+++ futureCaculator.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <p>Tien dau tu: </p><input type="text" name="soTien">
+    <p>Lai suat hang nam: </p><input type="text" name="laiSuat">
+    <p>So nam: </p><input type="text" name="soNam">
+    <input type="submit" value="Calculate">
+
+</form>
+</body>
+</html>
